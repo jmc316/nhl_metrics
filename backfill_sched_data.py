d701b9c6ec6fb5e91da560fef39fe345a392c773
Jared Carl <44181800+jmc316@users.noreply.github.com>
2026-08-25 16:55:16 -0400

    Adding penalty inputs to backfill

--- backfill_sched_data.py
+++ backfill_sched_data.py
@@ -13,7 +13,8 @@ def backfill():
     # load all schedule data
     sched_df = load_sched_df()
     new_data = {}
-    valid_event_types = ['shot-on-goal', 'missed-shot', 'blocked-shot']
+    valid_event_types = ['shot-on-goal', 'missed-shot', 'blocked-shot', 'penalty']
+    all_event_types = []
 
     for gameId in sched_df[cons.game_id_col].unique():
 
@@ -29,12 +30,19 @@ def backfill():
             'home_missed-shot': 0,
             'away_missed-shot': 0,
             'home_blocked-shot': 0,
-            'away_blocked-shot': 0
+            'away_blocked-shot': 0,
+            'home_penalty': 0,
+            'away_penalty': 0,
+            'home_pp_goal': 0,
+            'away_pp_goal': 0,
         }
 
         for play in game_data['plays']:
             event_type = play.get('typeDescKey')
             event_team = play.get('details', {}).get('eventOwnerTeamId')
+
+            if event_type not in all_event_types:
+                all_event_types.append(event_type)
 
             if event_type in valid_event_types:
                 if event_team == home_id:
@@ -44,6 +52,8 @@ def backfill():
                         new_data[gameId]['home_missed-shot'] += 1
                     elif event_type == 'blocked-shot':
                         new_data[gameId]['home_blocked-shot'] += 1
+                    elif event_type == 'penalty':
+                        new_data[gameId]['home_penalty'] += 1
                 elif event_team == away_id:
                     if event_type == 'shot-on-goal':
                         new_data[gameId]['away_shot-on-goal'] += 1
@@ -51,6 +61,27 @@ def backfill():
                         new_data[gameId]['away_missed-shot'] += 1
                     elif event_type == 'blocked-shot':
                         new_data[gameId]['away_blocked-shot'] += 1
+                    elif event_type == 'penalty':
+                        new_data[gameId]['away_penalty'] += 1
+
+        match_summary = nhl_client.game_center.match_up(game_id=gameId)
+
+        i = 0
+
+        while i < len(match_summary['summary']['scoring']):
+            goals_by_period = match_summary['summary']['scoring'][i]['goals']
+            if len(goals_by_period) == 0:
+                i += 1
+                continue
+            for goal_play in goals_by_period:
+                if goal_play.get('strength') == 'pp':
+                    if goal_play.get('isHome') == True:
+                        new_data[gameId]['home_pp_goal'] += 1
+                    else:
+                        new_data[gameId]['away_pp_goal'] += 1
+            i += 1
+
+        pass
 
     new_data_df = pd.DataFrame(new_data).T.reset_index(names=cons.game_id_col)
 
